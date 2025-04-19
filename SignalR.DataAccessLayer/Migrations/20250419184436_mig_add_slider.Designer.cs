8354f2247d5cc683ac553288910fdddc95351749
berkaycelik66 <berkaycelik66@hotmail.com>
2025-04-19 23:34:58 +0300

    api consumed in home page component partials

--- SignalR.DataAccessLayer/Migrations/20250419184436_mig_add_slider.Designer.cs
+++ SignalR.DataAccessLayer/Migrations/20250419184436_mig_add_slider.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SignalR.DataAccessLayer.Concrete;
 
@@ -11,9 +12,11 @@ using SignalR.DataAccessLayer.Concrete;
 namespace SignalR.DataAccessLayer.Migrations
 {
     [DbContext(typeof(SignalRContext))]
-    partial class SignalRContextModelSnapshot : ModelSnapshot
+    [Migration("20250419184436_mig_add_slider")]
+    partial class mig_add_slider
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -102,22 +105,10 @@ namespace SignalR.DataAccessLayer.Migrations
                     b.Property<string>("FooterDescription")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("FooterTitle")
-                        .HasColumnType("nvarchar(max)");
-
                     b.Property<string>("Location")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Mail")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("OpenDays")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("OpenDaysDescription")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("OpenHours")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Phone")
--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOurMenuComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOurMenuComponentPartial.cs
@@ -1,12 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.CategoryDtos;
+using SignalRWebUI.Dtos.ProductDtos;
+using SignalRWebUI.Dtos.SliderDtos;
+using static System.Net.WebRequestMethods;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultOurMenuComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultOurMenuComponentPartial(IHttpClientFactory httpClientFactory, IConfiguration configuration)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.Categories = await GetCategories();
+            ViewBag.Products = await GetProductsDetailed();
+
             return View();
+        }
+
+        private async Task<List<ResultCategoryDto>> GetCategories()
+        {
+            using var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7202/api/Category");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+            return values!;
+        }
+
+        private async Task<List<ResultProductWithCategoryDto>> GetProductsDetailed()
+        {
+            using var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7202/api/Product/ProductListWithCategory");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultProductWithCategoryDto>>(jsonData);
+            return values!;
         }
     }
 }
