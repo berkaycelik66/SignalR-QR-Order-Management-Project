5f8b3eb76681cb2bd527c57569e211c56577a00a
berkaycelik66 <berkaycelik66@hotmail.com>
2025-04-21 21:22:30 +0300

    basket operations added

---
+++ SignalRApi/Controllers/BasketController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.BasketDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketService _basketService;
+        private readonly IMapper _mapper;
+
+        public BasketController(IBasketService basketService, IMapper mapper)
+        {
+            _basketService = basketService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("GetBasketByMenuTableNumber")]
+        public IActionResult GetBasketByMenuTableNumber(int id)
+        {
+            var value = _mapper.Map<List<ResultBasketListWithProducts>>(_basketService.TGetBasketByMenuTableNumber(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBasket(CreateBasketDto createBasketDto)
+        {
+            var value = _mapper.Map<Basket>(createBasketDto);
+            _basketService.TAdd(value);
+            return Ok("Ürün Sepete Eklendi");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBasket(int id)
+        {
+            var value = _basketService.TGetByID(id);
+            if (value != null)
+            {
+                _basketService.TDelete(value);
+                return Ok("Ürün Sepetten Çıkarıldı");
+            }
+            return NotFound("İçerik bulunamadı");
+        }
+    }
+}
---
+++ SignalRApi/Mapping/BasketMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.BasketDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class BasketMapping : Profile
+    {
+        public BasketMapping()
+        {
+            CreateMap<Basket, CreateBasketDto>().ReverseMap();
+            CreateMap<Basket, ResultBasketListWithProducts>()
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product!.ProductName))
+                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.Product!.ImageUrl))
+                .ReverseMap();
+        }
+    }
+}
