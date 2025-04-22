c2a2f7b4aada8ab233552bfa43a18788ac3a8c15
berkaycelik66 <berkaycelik66@hotmail.com>
2025-04-22 18:13:14 +0300

    notification operations added

---
+++ SignalR.DataAccessLayer/EntityFramework/EfNotificationDal.cs
@@ -0,0 +1,53 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfNotificationDal : GenericRepository<Notification>, INotificationDal
+    {
+        public EfNotificationDal(SignalRContext context) : base(context)
+        {
+        }
+
+        public List<Notification> GetAllNotificationByStatusFalse()
+        {
+            using var context = new SignalRContext();
+            return context.Notifications.Where(x => x.Status == false).ToList();
+        }
+
+        public int NotificationCountByStatusFalse()
+        {
+            using var context = new SignalRContext();
+            return context.Notifications.Where(x => x.Status == false).Count();
+        }
+
+        public void NotificationStatusChangeToFalse(int id)
+        {
+            using var context = new SignalRContext();
+            var value = context.Notifications.Find(id);
+            if (value != null)
+            {
+                value!.Status = false;
+                context.SaveChanges();
+            }
+        }
+
+        public void NotificationStatusChangeToTrue(int id)
+        {
+            using var context = new SignalRContext();
+            var value = context.Notifications.Find(id);
+            if(value != null)
+            {
+                value!.Status = true;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ SignalRApi/Controllers/NotificationController.cs
@@ -0,0 +1,87 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.NotificationDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+        private readonly IMapper _mapper;
+
+        public NotificationController(INotificationService notificationService, IMapper mapper)
+        {
+            _notificationService = notificationService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult NotificationList()
+        {
+            var values = _notificationService.TGetListAll();
+            return Ok(values);
+        }
+
+        [HttpGet("NotificationCountByStatusFalse")]
+        public IActionResult NotificationCountByStatusFalse()
+        {
+            return Ok(_notificationService.TNotificationCountByStatusFalse());
+        }
+
+        [HttpGet("GetAllNotificationByStatusFalse")]
+        public IActionResult GetAllNotificationByStatusFalse()
+        {
+            return Ok(_notificationService.TGetAllNotificationByStatusFalse());
+        }
+
+        [HttpPost]
+        public IActionResult CreateNotification(CreateNotificationDto createNotificationDto)
+        {
+            var value = _mapper.Map<Notification>(createNotificationDto);
+            _notificationService.TAdd(value);
+            return Ok("Bildirim eklendi");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNotification(int id)
+        {
+            var value = _notificationService.TGetByID(id);
+            _notificationService.TDelete(value);
+            return Ok("Bildirim Silindi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetNotification(int id)
+        {
+            var value = _notificationService.TGetByID(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateNotification(UpdateNotificationDto updateNotificationDto)
+        {
+            var value = _mapper.Map<Notification>(updateNotificationDto);
+            _notificationService.TUpdate(value);
+            return Ok("Bildirim Güncellendi");
+        }
+
+        [HttpPut("NotificationStatusChangeToTrue/")]
+        public IActionResult NotificationStatusChangeToTrue([FromBody]int id)
+        {
+            _notificationService.TNotificationStatusChangeToTrue(id);
+            return Ok("Durum True Olarak Değiştirildi");
+        }
+
+        [HttpPut("NotificationStatusChangeToFalse/")]
+        public IActionResult NotificationStatusChangeToFalse([FromBody] int id)
+        {
+            _notificationService.TNotificationStatusChangeToFalse(id);
+            return Ok("Durum False Olarak Değiştirildi");
+        }
+    }
+}
