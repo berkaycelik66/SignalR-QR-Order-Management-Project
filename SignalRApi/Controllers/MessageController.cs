27a1379faabca2f35f10c051747ebea0027f80a1
berkaycelik66 <berkaycelik66@hotmail.com>
2025-04-26 22:50:32 +0300

    contact us partial view added

---
+++ SignalRApi/Controllers/MessageController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.MessageDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly IMessageService _messageService;
+        private readonly IMapper _mapper;
+
+        public MessageController(IMessageService messageService, IMapper mapper)
+        {
+            _messageService = messageService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult MessageList()
+        {
+            var value = _mapper.Map<List<ResultMessageDto>>(_messageService.TGetListAll());
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateMessage(CreateMessageDto createMessageDto)
+        {
+            createMessageDto.Status = false;
+            createMessageDto.Date = DateTime.Now;
+            var value = _mapper.Map<Message>(createMessageDto);
+            _messageService.TAdd(value);
+            return Ok("Mesaj Eklendi");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMessage(int id)
+        {
+            var value = _messageService.TGetByID(id);
+            _messageService.TDelete(value);
+            return Ok("Mesaj Silindi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMessage(int id)
+        {
+            var value = _messageService.TGetByID(id);
+            return Ok(_mapper.Map<GetMessageDto>(value));
+        }
+        [HttpPut]
+        public IActionResult UpdateMessage(UpdateMessageDto updateMessageDto)
+        {
+            updateMessageDto.Status = false;
+            var value = _mapper.Map<Message>(updateMessageDto);
+            _messageService.TUpdate(value);
+            return Ok("Mesaj Güncellendi");
+        }
+    }
+}
